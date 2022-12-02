b47d43e4d0ecc0aa68d9cb1defe8d8490511f797
Manuel Sgobino <msgobino@netcologne.de>
2022-12-02 07:52:20 +0100

    day 01 cleaned and consolidated to solve()

--- day01.js
+++ day01.js
@@ -4,25 +4,16 @@ const parseInput = input => {
 	return input.split('\n\n').map(l => l.split('\n').map(v => parseInt(v)))
 }
 
-const solve = (isPart2, input) => {
-	return input.reduce((acc, cur) => {
-		const r = cur.reduce((a, c) => a + c, 0)
-		return (r > acc) ? r : acc
-	}, 0)
-}
-
-const part1 = input => {
-	return solve(false, parseInput(input))
-}
-
-const part2 = input => {
-	const vals = parseInput(input).reduce((acc, cur) => {
-		const r = cur.reduce((a, c) => a + c, 0)
-		acc.push(r)
+const solve = (tops, input) => parseInput(input).reduce((acc, cur) => {
+		acc.push(cur.reduce((a, c) => a + c, 0))
 		return acc
 	}, [])
-	vals.sort((a, b) => b - a)
-	return vals.slice(0, 3).reduce((a, c) => a + c, 0)
-}
+	.sort((a, b) => b - a)
+	.slice(0, tops)
+	.reduce((a, c) => a + c, 0)
+
+const part1 = input => solve(1, input)
+
+const part2 = input => solve(3, input)
 
 module.exports = { part1, part2 }
