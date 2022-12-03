0e1fbcdebf82badcd0810e54720670754a927fcd
Manuel Sgobino <msgobino@netcologne.de>
2022-12-03 07:07:06 +0100

    day 03 cleaned up

--- day03.js
+++ day03.js
@@ -1,14 +1,9 @@
 'use strict'
 
-const parseInput = input => {
-    return input.split('\n')
-        .map(l => [l.slice(0, l.length / 2), l.slice(l.length / 2)])
-}
-
 const worth = '0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-const part1 = input => {
-  return parseInput(input)
+const part1 = input => input.split('\n')
+        .map(l => [l.slice(0, l.length / 2), l.slice(l.length / 2)])
         .reduce((a, c) => {
             for(const s of c[0].split('')) {
                 if (c[1].indexOf(s) != -1) {
@@ -19,17 +14,13 @@ const part1 = input => {
             return a
         }, []) 
         .reduce((a, c) => a + worth.indexOf(c), 0)
-}
 
 const part2 = input => {
-    const rucksacks = parseInput(input)
+    const sacks = input.split('\n')
     const badges = []
-    for (let i = 0; i < rucksacks.length; i += 3){
+    for (let i = 0; i < sacks.length; i += 3){
         for (const s of worth.split('')) {
-            if ((rucksacks[i][0] + rucksacks[i][1]).indexOf(s) != -1
-                && (rucksacks[i + 1][0] + rucksacks[i + 1][1]).indexOf(s) != -1
-                && (rucksacks[i + 2][0] + rucksacks[i + 2][1]).indexOf(s) != -1
-            ) {
+            if (sacks[i].indexOf(s) != -1 && sacks[i + 1].indexOf(s) != -1 && sacks[i + 2].indexOf(s) != -1) {
                 badges.push(s)
                 break
             }
