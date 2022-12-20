b6ce4b5763a13eaa3d0760e4e0ef56390da5882f
Manuel Sgobino <msgobino@netcologne.de>
2022-12-20 21:57:49 +0100

    day 20 challenge commit

--- day20.js
+++ day20.js
@@ -1,31 +1,23 @@
 'use strict'
 
-const parseInput = (input, mul) => input.split('\n').map(Number)
-
 const solve = (isPart2, input) => {
-	input = parseInput(input, isPart2 ? 811589153 : 1)
-	const mixed = input.map((n, i) => [n, i]), l = isPart2 ? 10 : 1, m = mixed.length
+    const key = isPart2 ? 811589153 : 1
+	const initial = input.split('\n').map(v => v * key)
+    const mixed = input.split('\n').map((v, i) => ({num: v * key, id: i}))
+	const l = isPart2 ? 10 : 1, m = initial.length
 	for (let j = 0; j < l; j++) {
-		console.log([input.slice(0, 3), mixed.slice(0, 3)])
-		for (let x = 0; x < m; x++) {
-			const i = mixed.findIndex(f => f[1] == x)
-			const n = mixed[i][0]
-			mixed.splice(i, 1)
-			mixed.splice((i + n) % m, 0, [n, x])
-		}
+        for (let i = 0; i < m; i++) {
+            const id = mixed.findIndex(f => f.id === i)
+            mixed.splice(id, 1)
+            mixed.splice((initial[i] + id) % mixed.length, 0, {num: initial[i], id: i})
+        }
 	}
-	console.log(mixed.slice(0, 3))
-	return mixed.map(e => e[0])
+    const zero = mixed.findIndex(x => x.num === 0)
+    return [1000, 2000, 3000].reduce((a, c) => a + mixed[(c + zero) % mixed.length].num, 0)
 }
 
-const calcCoords = mixed => {
-	const zero = mixed.findIndex(f => f == 0), m = mixed.length
-	console.log([zero, mixed[zero], m, (zero + 1000) % m, (zero + 2000) % m,mixed[(zero + 1000) % m] , mixed[(zero + 2000) % m] , mixed[(zero + 3000) % m]])
-	return mixed[(zero + 1000) % m] + mixed[(zero + 2000) % m] + mixed[(zero + 3000) % m]
-}
+const part1 = input => solve(false, input)
 
-const part1 = input => calcCoords(solve(false, input))
-
-const part2 = input => calcCoords(solve(true, input))
+const part2 = input => solve(true, input)
 
 module.exports = { part1, part2 }
